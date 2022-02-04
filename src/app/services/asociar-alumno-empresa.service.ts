6e97f6f3d98df9235ae09ef909f1ec628eaae516
Laura <lauramorenoramos97@gmail.com>
2022-02-04 09:01:44 +0100

    rutas de servicio cambiadas, para comodidad con la variable ruta

--- src/app/services/asociar-alumno-empresa.service.ts
+++ src/app/services/asociar-alumno-empresa.service.ts
@@ -10,28 +10,28 @@ import * as FileSaver from 'file-saver';
 })
 export class AsociarAlumnoEmpresaService {
 
-
+public ruta='http://127.0.0.1:8000/api/';
   constructor(private http: HttpClient) {
 
    }
 
   public solicitarAlumnos(dniTutor: string){
-    let url: string = "http://127.0.0.1:8000/api/solicitarAlumnosSinEmpresa/"+dniTutor;
+    let url: string = this.ruta+"solicitarAlumnosSinEmpresa/"+dniTutor;
     return this.http.get<Alumno[]>(url)
   }
 
   public solicitarEmpresas(dniTutor: string){
-    let url: string = "http://127.0.0.1:8000/api/solicitarEmpresasConAlumnos/"+dniTutor;
+    let url: string = this.ruta+"solicitarEmpresasConAlumnos/"+dniTutor;
     return this.http.get<Empresa[]>(url)
   }
 
   public solicitarNombreCiclo(dniTutor: string){
-    let url: string = "http://127.0.0.1:8000/api/solicitarNombreCiclo/"+dniTutor;
+    let url: string = this.ruta+"solicitarNombreCiclo/"+dniTutor;
     return this.http.get(url)
   }
 
   public asignarAlumnos(dniTutor: string, empresas: Empresa[]){
-    let url: string = "http://127.0.0.1:8000/api/actualizarEmpresaAsignadaAlumno";
+    let url: string = this.ruta+"actualizarEmpresaAsignadaAlumno";
     let headers = new HttpHeaders({
       'Content-Type': 'application/json',
     });
@@ -46,7 +46,7 @@ export class AsociarAlumnoEmpresaService {
 
   public generarAnexo(dni:string){
     let dato= {dni_tutor:dni};
-    const url: string="http://127.0.0.1:8000/api/relleno";
+    const url: string= this.ruta+"relleno";
 
     return this.http.post(url,dato, {responseType:'arraybuffer'});
   }
