d13b823f09aa2330c650e05f69ffe53c904a0d25
DaniJCoello <daniel.jimenezcoello@gmail.com>
2022-02-24 18:12:26 +0100

    Implementado diálogo de confirmación delete Falta universal

--- src/app/modules/data-management/gestion-empresas/gestion-empresas.component.ts
+++ src/app/modules/data-management/gestion-empresas/gestion-empresas.component.ts
@@ -4,6 +4,9 @@ import { Empresa } from 'src/app/models/empresa';
 import { CrudEmpresasService } from 'src/app/services/crud-empresas.service';
 import { LoginStorageUserService } from 'src/app/services/login.storageUser.service';
 import { ModalEmpresaComponent } from '../modal-empresa/modal-empresa.component';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { DialogService } from 'src/app/services/dialog.service';
+import { ConfirmDialogComponent } from 'src/app/shared/confirm-dialog/confirm-dialog.component';
 
 @Component({
   selector: 'app-gestion-empresas',
@@ -18,10 +21,12 @@ export class GestionEmpresasComponent implements OnInit {
   constructor(
     private crudEmpresasService: CrudEmpresasService,
     private modal: NgbModal,
-    private storageUser: LoginStorageUserService
+    private storageUser: LoginStorageUserService,
+    private dialog: MatDialog,
+    public dialogService: DialogService
   ) {
     this.usuario = storageUser.getUser();
-      this.dniTutor = this.usuario?.dni
+    this.dniTutor = this.usuario?.dni;
   }
 
   ngOnInit(): void {
@@ -36,15 +41,15 @@ export class GestionEmpresasComponent implements OnInit {
     this.crudEmpresasService.getEmpresas(this.dniTutor!).subscribe({
       next: (empresas) => {
         this.empresas = empresas;
-        this.empresas.forEach(empresa => {
+        this.empresas.forEach((empresa) => {
           this.crudEmpresasService.getRepresentante(empresa.id).subscribe({
             next: (representante) => {
               empresa.representante = representante;
-            }
-          })
-        })
-      }
-    })
+            },
+          });
+        });
+      },
+    });
   }
 
   /**
@@ -54,6 +59,13 @@ export class GestionEmpresasComponent implements OnInit {
    * @author Dani J. Coello <daniel.jimenezcoello@gmail.com>
    */
   public mostrarEmpresa(empresa: Empresa, editar: boolean) {
+    // await this.dialogService.confirmacion(
+    //   "Eliminar registro",
+    //   `¿Está seguro de que quiere eliminar el registro de la empresa ${empresa.nombre}?`
+    //   ).then( resp => {
+    //     console.log(resp);
+    //   })
+
     this.modal.open(ModalEmpresaComponent, {
       size: 'md',
       backdrop: 'static',
@@ -67,12 +79,24 @@ export class GestionEmpresasComponent implements OnInit {
    * @param idEmpresa el ID de la empresa a eliminar
    * @author Dani J. Coello <daniel.jimenezcoello@gmail.com>
    */
-  public deleteEmpresa(idEmpresa: string) {
-    this.crudEmpresasService.deleteEmpresa(idEmpresa).subscribe({
-      next: (response: any) => {
-        this.getEmpresas();
-        console.log(response.message);
-      },
-    });
+  public deleteEmpresa(empresa: Empresa) {
+    this.dialog
+      .open(ConfirmDialogComponent, {
+        data: {
+          title: 'Eliminar registro',
+          message: `¿Está seguro de que quiere eliminar el registro de la empresa ${empresa.nombre}?`,
+        },
+        width: '400px',
+      })
+      .afterClosed().subscribe((res) => {
+        if (res.respuesta) {
+          this.crudEmpresasService.deleteEmpresa(empresa.id).subscribe({
+            next: (response: any) => {
+              this.getEmpresas();
+              console.log(response.message);
+            },
+          });
+        }
+      });
   }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,17 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { SharedRoutingModule } from './shared-routing.module';
+import { ConfirmDialogComponent } from './confirm-dialog/confirm-dialog.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ConfirmDialogComponent
+  ],
   imports: [
     CommonModule,
     HttpClientModule,
     SharedRoutingModule
   ],
   exports: [
-    CommonModule
+    CommonModule,
+    ConfirmDialogComponent
   ]
 })
 export class SharedModule { }
