aa61925ff61dca5fbe5c4686cb6a71c828b6bff4
David Sánchez Barragán <davidsanchezbarragan@gmail.com>
2022-03-07 13:22:09 +0100

    Commit pre-produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,11 @@
 export const environment = {
   production: true,
-  apiUrl: "http://localhost:5566",
+  apiUrl: "http://localhost:8000/api/jefatura/recibirCSV",
+  alumnos: "alumnos",
+  materias: "materias",
+  matriculas: "matriculas",
+  notas: "notas",
+  unidades: "unidades",
+  profesores: "profesores",
   apiUrlFirma: "http://localhost:8000/api/"
 };
