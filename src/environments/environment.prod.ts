df04978ef5121bcf67e43465b5565d7aa9da6b3e
David Sánchez Barragán <davidsanchezbarragan@gmail.com>
2022-03-07 13:54:33 +0100

    Commit de prueba añadiendo la propiedad TEST a enviornment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,5 +7,6 @@ export const environment = {
   notas: "notas",
   unidades: "unidades",
   profesores: "profesores",
-  apiUrlFirma: "http://localhost:8000/api/"
+  apiUrlFirma: "http://localhost:8000/api/",
+  test: "TEST"
 };
