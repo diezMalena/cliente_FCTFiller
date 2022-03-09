0f283223ddfb4e553c7211899124653adcc60a0f
DaniJCoello <daniel.jimenezcoello@gmail.com>
2022-03-09 14:29:32 +0100

    Empezado manual de gestión de empresas

--- src/app/modules/data-management/gestion-empresas/gestion-empresas.component.ts
+++ src/app/modules/data-management/gestion-empresas/gestion-empresas.component.ts
@@ -14,6 +14,7 @@ import { DialogService } from 'src/app/services/dialog.service';
 import { DataTableDirective } from 'angular-datatables';
 import { Subject } from 'rxjs';
 import { ToastrService } from 'ngx-toastr';
+import { ManualGestionEmpresasComponent } from '../../manuales/manual-gestion-empresas/manual-gestion-empresas.component';
 
 @Component({
   selector: 'app-gestion-empresas',
@@ -133,8 +134,8 @@ export class GestionEmpresasComponent
         },
         error: (err: any) => {
           console.log(err);
-          this.toastr.error(err.error.message, err.error.title)
-        }
+          this.toastr.error(err.error.message, err.error.title);
+        },
       });
     }
   }
@@ -148,5 +149,13 @@ export class GestionEmpresasComponent
       this.empresas = array;
       this.rerender();
     });
+  }
+
+  /**
+   * Abre un modal de ayuda
+   * @author Dani J. Coello <daniel.jimenezcoello@gmail.com>
+   */
+  public abrirAyuda(): void {
+    this.modal.open(ManualGestionEmpresasComponent, { size: 'lg' });
   }
 }
